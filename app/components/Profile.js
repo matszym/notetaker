84596d4489a8c9b4fa8f83b0f1d0cd778779944a
Mateusz Szymański <hello`at`mateuszszymanski`dot`eu (remove ` from address, at === @, dot === .)>
2016-09-27 19:22:57 +0200

    Hookup firebase

--- app/components/Profile.js
+++ app/components/Profile.js
@@ -1,25 +1,46 @@
 const React = require('react');
 const Router = require('react-router');
+const ReactFireMixin = require('reactfire');
+const firebase = require('firebase');
+
+const firebaseConfig = require('../config/firebase.js');
+
+const Repos = require('./Github/Repos.js');
+const UserProfile = require('./Github/UserProfile.js');
+const Notes = require('./Notes/Notes.js');
 
 const Profile = React.createClass({
+  mixins: [ReactFireMixin],
   getInitialState() {
     return {
-      notes: [],
-      bio: {},
-      repos: []
+      notes: [1,2,3],
+      bio: {
+        name: 'Tyler McGinnis'
+      },
+      repos: ['a', 'b', 'c']
     }
+  },
+  componentDidMount() {
+    firebase.initializeApp(firebaseConfig);
+
+    this.ref = firebase.database().ref('/');
+    const childRef = this.ref.child(this.props.params.username);
+    this.bindAsArray(childRef, 'notes');
+  },
+  componentWillUnmount() {
+    this.unbind('notes');
   },
   render() {
     return (
       <div className="row">
         <div className="col-md-4">
-          User Profile Component
+          <UserProfile username={this.props.params.username} bio={this.state.bio} />
         </div>
         <div className="col-md-4">
-          Repos Component
+          <Repos username={this.props.params.username} repos={this.state.repos} />
         </div>
         <div className="col-md-4">
-          Notes Profile Component
+          <Notes username={this.props.params.username} notes={this.state.notes} />
         </div>
       </div>
     )
